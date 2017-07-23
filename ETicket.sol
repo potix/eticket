30bb12d1ed9aeaae45006e8b50ccbdc11790c58c
potix <poti.dog@gmail.com>
2017-07-24 06:41:26 +0900

    update

--- ETicket.sol
+++ ETicket.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
 import "./StandardToken.sol";
-import "ownership/Ownable.sol";
+import "./Ownable.sol";
 
 contract ETicket is StandardToken, Ownable {
     string public name = "ETicketToken"; 
@@ -13,6 +13,103 @@ contract ETicket is StandardToken, Ownable {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
-    
+
+    // publich ticket
+    struct publishEventTicker {
+        uint ticketId;
+        uint ticketSerialNumber;
+        uint discard;
+        address owner;
+    }
+    mapping (address => mapping(uint => mapping(uint => publishEventTicket))) publishEventTickets;
+
+    // publich ticket group
+    struct publishEventTicketGroup {
+        uint ticketGroupId;
+        uint lastTicketIndex;
+        uint discard;
+    }
+    mapping (address => mapping(uint => mapping(uint => publichEventTicketGroup))) publishEventTicketGroups;
+
+    // publich event
+    struct publishEvent {
+        uint eventId;
+        string name;
+        string description;
+        uint startTime;
+        uint endTime;
+        string place;
+        string mapUrl;
+        uint   lastTicketGroupsIndex;
+        uint   lastTicketSerialNumber;
+        uint   stop;
+    }
+    mapping (address => mapping(uint => evnt)) publishEvents;
+
+    // ticket
+    struct ticker {
+        address publisher;
+        uint eventId;
+        uint ticketGroupId;
+        uint ticketId;
+        uint price;
+        uint owner;
+    }
+    mapping (address => mapping(uint => Ticket)) Tickets;
+
+    // user 
+    struct user {
+        string name;
+        string email;
+        uint   lastEventsIndex;
+        uint   lastTicketsIndex;
+    }
+    mapping (address => user) users;
+
+
+
+    // user operation
+    function CreateUser() {
+        
+    }
+
+    function ModifytUser() {
+        
+    }
+
+
+
+    // event operation
+    function CreateEvent() {
+        
+    }
+
+    function ModifyEvent() {
+        
+    }
+
+    function StopEvent() {
+        
+    }
+
+
+
+    // event ticket
+    function CreateTicket() {
+        
+    }
+
+    function DiscardTicketGroup() {
+        
+    }
+
+    function DiscardTicket() {
+        
+    }
+
+
+
+ 
+ 
     
 }
