41d3f67cfd09782f31b68b315db79f9b3ea31081
potix <poti.dog@gmail.com>
2017-08-03 01:09:16 +0900

    update

--- contract/Convert.sol
+++ contract/Convert.sol
@@ -1,20 +1,24 @@
 pragma solidity ^0.4.11;
 
-import './Convert.sol';
+library Convert {
 
-library ConvertTest {
-	function test1() returns (uint, string) {
-		bytes32  b = 254;
-		return (uint(b), Convert.bytes32ToHexString(b));
+	function getHexChar(byte _b) private returns (byte) {
+		if (_b >= 0 && _b <= 9) {
+			return byte(uint8(_b) + uint8(0x30));
+		} else {
+			return byte(uint8(_b) + uint8(0x37));
+		}
 	}
 
-	function test2() returns (uint, string) {
-		bytes32  b = 65534;
-		return (uint(b), Convert.bytes32ToHexString(b));
-	}
-	
-	function test3() returns (uint, string) {
-		bytes32  b = sha3(1);
-		return (uint(b), Convert.bytes32ToHexString(b));
+	function bytes32ToHexString(bytes32 _b) internal returns (string) {
+		bytes memory _bs = new bytes(64);
+		for (uint i; i < 32; i++) {
+		    var a = (_b[i] & 0xf0) >> 4;
+		    var b = _b[i] & 0x0f;
+			_bs[i * 2] = getHexChar((_b[i] & 0xf0) >> 4);
+			_bs[(i * 2) + 1] = getHexChar(_b[i] & 0x0f); 
+		}
+		return string(_bs);
 	}
 }
+
