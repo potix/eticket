fb6e7a0e18fdd3f5c0a423bec9e54176e5eaeb2e
potix <poti.dog@gmail.com>
2017-08-06 05:36:38 +0900

    update

--- contract/ETicket.sol
+++ contract/ETicket.sol
@@ -1,5 +1,13 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.14;
 
-contract ETicket is Token, Ticket {
-	
+import "./Token.sol";
+
+contract ETicket is Token {
+    address ticketDB;
+    
+    function ETicket(address _tokenDB, address _ticketDB) Token(_tokenDB) {
+        require(_ticketDB != address(0));
+        name = "xxx";
+    }
 }
+
