744578e2c051e11cc8862773f652d82a9d528a38
potix <poti.dog@gmail.com>
2017-08-11 16:47:10 +0900

    update

--- contract/ETicketInterface.sol
+++ contract/ETicketInterface.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.14;
 
-contract TicketInterface {
+contract ETicketInterface {
     function createUser(string _name, string _email, string _profile) returns (uint256);
 }
+
 
 
